6f8ff96953b39c1c57761b068683a28741f14c78
unknown <huy.cao2001@hcmut.edu.vn>
2022-08-16 16:13:15 +0700

    Add logging and logging level config.

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,21 +1,23 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ch.qos.logback.classic.Logger;
+
 
 @Controller
 public class LoginController {
 	
-	///login => com.in28minutes.springboot.myfirstwebapp.login.LoginController => login.jsp
-	
-	//http://localhost:8080/login?name=Ranga
+	private org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
 	//Model
 	@RequestMapping("login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
+		logger.debug("Request param is " + name);
 		System.out.println("Request param is " + name); //NOT RECOMMENDED FOR PROD CODE
 		return "login";
 	}
