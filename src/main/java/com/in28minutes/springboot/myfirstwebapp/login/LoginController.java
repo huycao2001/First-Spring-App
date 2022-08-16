0dc5ac3b56845108a7456244dc3bd4f90666dc07
unknown <huy.cao2001@hcmut.edu.vn>
2022-08-16 20:27:25 +0700

    Using Appropriate RequestMethod : GET and POST.

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -3,7 +3,9 @@ package com.in28minutes.springboot.myfirstwebapp.login;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.ui.context.Theme;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import ch.qos.logback.classic.Logger;
@@ -14,11 +16,19 @@ public class LoginController {
 	
 	private org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
 	//Model
-	@RequestMapping("login")
-	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
-		logger.debug("Request param is " + name);
-		System.out.println("Request param is " + name); //NOT RECOMMENDED FOR PROD CODE
+	@RequestMapping(value="login", method = RequestMethod.GET)
+	public String gotoLoginPage() {
+
 		return "login";
+	}
+	
+	
+	@RequestMapping(value="login", method = RequestMethod.POST)
+	// The function below acts like the action in php. 
+	// when we post the data, the function will redirect to the welcome page. 
+	public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+		model.put("name", name);
+		model.put("password", password); 
+		return "welcome";
 	}
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title> Welcome page</title>
+	</head>
+	<body>
+		<div> Welcome to the welcome page ! </div>
+		<div> Your name is ${name} </div>
+		<div> Your password is ${password} </div>
+		
+		
+	</body>
+</html>
