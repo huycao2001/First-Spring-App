07130d402bea40fcc565a0b2477cf1a8cfbdd221
unknown <huy.cao2001@hcmut.edu.vn>
2022-08-17 00:52:53 +0700

    Add a simple Authentication Service.

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -14,7 +14,20 @@ import ch.qos.logback.classic.Logger;
 @Controller
 public class LoginController {
 	
-	private org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
+	//private org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private AuthenticationService authenticationService;
+	
+	
+	
+	// Constructor injection 
+	// Spring will search for the service and autowire it to this member
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
+
+
 	//Model
 	@RequestMapping(value="login", method = RequestMethod.GET)
 	public String gotoLoginPage() {
@@ -27,8 +40,14 @@ public class LoginController {
 	// The function below acts like the action in php. 
 	// when we post the data, the function will redirect to the welcome page. 
 	public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
-		model.put("name", name);
-		model.put("password", password); 
-		return "welcome";
+		if(authenticationService.authenticate(name, password)) {
+			model.put("name", name);
+			model.put("password", password); 
+			return "welcome";
+		}
+		
+		model.put("errorMessage", "Invalid credentials, please try again.");
+		
+		return "login";
 	}
 }
