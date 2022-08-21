7abfb88635ed406a748d2ee709e4a78530e184c4
unknown <huy.cao2001@hcmut.edu.vn>
2022-08-22 02:40:05 +0700

    Modify update functions : Allows update and add Date.

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -49,7 +49,7 @@ public class TodoController {
 		
 		String username = (String)model.get("name");
 		todoService.addTodo(username, todo.getDescription(), 
-				LocalDate.now().plusYears(1), false);
+				todo.getTargetDate(), false);
 		return "redirect:list-todos";
 	}
 	
